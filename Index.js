7460a7362af879792773dad8d0e90b6ae3f33828
Nikhil <nikhilmasurkar1997@gmail.com>
2022-07-27 19:31:36 +0530

    done

--- Index.js
+++ Index.js
@@ -32,13 +32,13 @@ function displayTODO() {
     listArr.forEach((element, index) => {
         newDataTag += `<tr class="data_list">
         <th>
-        <label>${index + 1}</label>
+        <label><input class="check${index}" type="checkbox" value="">${index + 1}</label>
       </th>
         <td id='todo${index}'>${element}</td>
         <td class="Process_statement${index}">In progress</td>
         <td>
         <button onclick= deletetask(${index}) id="delete" type="submit" class="btn btn-danger">Delete</button>
-        <button onclick= completeTask(${index})  id="Finish" type="submit" class="btn btn-success ms-1">Finished</button>
+        <button onclick= completeTask(${index}) class="Finished${index}" id="Finish" type="submit" class="btn btn-success ms-1">Finished</button>
         </td>
     </tr>`
     })
@@ -55,24 +55,27 @@ function completeTask(index) {
     let Process_statement = document.querySelector(`.Process_statement${index}`);
     let items = list.getElementsByTagName("tr");
     let arr = [...items]
-    for (let i = 0; i<arr.length;i++) {
-         
+    //    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+
         if (i == index) {
             index = i;
             console.log()
-           
+
             if (completeFlag) {
                 Process_statement.innerHTML = "Completed"
                 Process_statement.style.color = "red"
                 Process_statement.style.textDecoration = "line-through 2px solid ";
-                document.querySelectorAll('Finish').textContent = "Unfinished"
+                document.querySelector(`.Finished${index}`).textContent = "Unfinished"
 
-                document.querySelectorAll(".check").checked = true;
+                document.querySelector(`.check${index}`).checked = true;
                 completeFlag = false;
             } else {
                 Process_statement.innerHTML = "In Progress"
                 Process_statement.style.color = "black"
+                document.querySelector(`.check${index}`).checked = false;
                 Process_statement.style.textDecoration = "none";
+                document.querySelector(`.Finished${index}`).textContent = "Finished"
                 completeFlag = true;
             }
         }
